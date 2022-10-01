8f5c5e013aad624ab0cc70819af35e5efacbf000
Spiridonov <dimsp1369@gmail.com>
2022-10-01 11:33:08 -0700

    add storybook config

---
+++ config/storybook/preview.js
@@ -0,0 +1,19 @@
+import { addDecorator } from '@storybook/react';
+import { Theme } from '../../src/app/providers/ThemeProvider';
+import { StyleDecorator } from '../../src/shared/config/storybook/Decorators/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/Decorators/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/Decorators/RouterDecorator';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+addDecorator(StyleDecorator)
+addDecorator(ThemeDecorator(Theme.LIGHT))
+addDecorator(RouterDecorator)
