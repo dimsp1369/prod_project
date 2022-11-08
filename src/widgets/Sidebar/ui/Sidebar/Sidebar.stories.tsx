e288a6e3276efcded0929c4927e153e9d00c847b
Spiridonov <dimsp1369@gmail.com>
2022-11-07 21:50:07 -0800

    update screenshots

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { Theme } from 'app/providers/ThemeProvider';
 import { ThemeDecorator } from 'shared/config/storybook/Decorators/ThemeDecorator';
+import { StoreDecorator } from 'shared/config/storybook/Decorators/StoreDecorator';
 import { Sidebar } from './Sidebar';
 
 export default {
@@ -17,8 +18,10 @@ const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [StoreDecorator({ user: { authData: {} } })];
 
 export const Dark = Template.bind({});
 Dark.args = {};
 
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [StoreDecorator({ user: { authData: {} } })];
